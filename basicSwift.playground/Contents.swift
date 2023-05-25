0a3f342e9591f0c84b3c8b5aa8bd5e89b7ba04f6
Bilawal Haesri <billawalhaesri@gmail.com>
2023-05-25 19:52:16 +0700

    Basic Swift Learned

--- basicSwift.playground/Contents.swift
+++ basicSwift.playground/Contents.swift
@@ -1,4 +1,40 @@
 import Cocoa
 
-let name = "Bilawal"
-print(name)
+//Declaring Variable and Constants
+let constant = 0
+var variable = 1
+var multipleVar = 2, x = 3, y = 5
+
+//Type Annotations
+var name: String
+name = "Bill"
+//Multiple Type Annotations
+var red, green, blue: String
+
+//Declaring Variable and constants using unicode
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+//Change variable value
+var country: String
+country = "England"
+country = "Spain"
+
+//Print Constants and Variables
+print(country)
+
+//String Interpolation
+print("Hello im \(name) from \(country)")
+
+//This is a single line comment
+
+/*This is a
+ Multi-Line Comment*/
+
+/*This is first
+ Multi Line Comment /* Inside it there is a
+                     nested multi Line Comment*/*/
+
+//Semicolon
+var fruit = "Grapes"; print(fruit)
