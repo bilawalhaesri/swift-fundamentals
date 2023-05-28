25f7e417736ee6493f2dc4c7094daaeed63e791b
Bilawal Haesri <billawalhaesri@gmail.com>
2023-05-28 21:47:29 +0700

    Swift basic Exercise Completed

--- basicExercise.playground/Contents.swift
+++ basicExercise.playground/Contents.swift
@@ -1,3 +1,13 @@
-import UIKit
+import Cocoa
 
-var greeting = "Hello, playground"
+let day = "Monday"
+let dailyTemp = 75
+print("Today is \(day)")
+print ("The temperature on \(day) is \(dailyTemp)°F")
+var temp = 70
+print("The temperature on \(day) morning is \(temp)°F")
+temp = 80
+print("The temperature on \(day) evening is \(temp)°F")
+let weeklyTemp = 75
+temp = weeklyTemp
+print("The average temperature on this week is \(temp)°F")
