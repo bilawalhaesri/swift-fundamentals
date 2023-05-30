41ab1579fc77b43f41014b006bd99aa8c957fb3f
Bilawal Haesri <billawalhaesri@gmail.com>
2023-05-31 03:26:25 +0700

    String Exercise files added

--- stringExercise.playground/Contents.swift
+++ stringExercise.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+import Cocoa
 
-var greeting = "Hello, playground"
+let instruction = """
+Suppose you are creating a calendar app that works with different time zones. You will use strings to determine the current day of the week and the current time and time zone.
+"""
+print(instruction)
+let day = "Monday"
+print("Today is \(day)")
+let hour = 6
+let minutes = 15
+let period = "PM"
+var time = String(hour) + ":" + String(minutes) + " " + period
+print("it is \(time)")
+print("it is \(time) on \(day)")
+let timeZone = "PST"
+time += " \(timeZone)"
+print("it is \(time) on \(day)")
+
+let shortDay = day.prefix(3)
+print("today is \(shortDay)")
+print("it is \(time) on \(shortDay)")
